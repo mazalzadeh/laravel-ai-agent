c3359d94a1daa3429e45147cca23c85fd086f316
mazalzade <maryam_mazalzadeh@yahoo.com>
2026-06-19 13:58:00 +0330

    add Rag chat api

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\ChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SemanticSearchController;
+use App\Http\Controllers\RagChatController;
 
-Route::post('/fake-openai/chat', function(Request $request){
+Route::post('/fake-openai/chat', function (Request $request) {
 
     $message = $request->input('messages.0.content');
 
@@ -27,6 +28,7 @@ Route::post('/fake-openai/chat', function(Request $request){
     );
 });
 
-Route::post('/chat',[ChatController::class, 'chat']);
+Route::post('/chat', [ChatController::class, 'chat']);
 Route::post('/chat/stream', [ChatController::class, 'stream']);
 Route::post('/semantic-search', SemanticSearchController::class);
+Route::post('/rag-chat', RagChatController::class);
